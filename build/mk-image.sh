c173b93fc32a59ccab5a7692c78ea73ef1ab5830
wit_fanghao <wit_fanghao@163.com>
2021-11-25 23:25:38 +0800

    shell modify

--- build/mk-image.sh
+++ build/mk-image.sh
@@ -4,18 +4,16 @@ LOCALPATH=$(pwd)
 OUT=${LOCALPATH}/out
 TOOLPATH=${LOCALPATH}/rkbin/tools
 EXTLINUXPATH=${LOCALPATH}/build/extlinux
-CHIP=""
 TARGET=""
 ROOTFS_PATH=""
-BOARD=""
 
 PATH=$PATH:$TOOLPATH
 
 source $LOCALPATH/build/partitions.sh
 
 usage() {
-	echo -e "\nUsage: build/mk-image.sh -c rk3399 -t system -r rk-rootfs-build/linaro-rootfs.img \n"
-	echo -e "       build/mk-image.sh -c rk3399 -t boot -b rockpi4b\n"
+	echo -e "build/mk-image.sh -t system -r rk-rootfs-build/linaro-rootfs.img \n"
+	echo -e "build/mk-image.sh -t boot\n"
 }
 finish() {
 	echo -e "\e[31m MAKE IMAGE FAILED.\e[0m"
@@ -26,61 +24,37 @@ trap finish ERR
 OLD_OPTIND=$OPTIND
 while getopts "c:t:r:b:h" flag; do
 	case $flag in
-		c)
-			CHIP="$OPTARG"
-			;;
 		t)
 			TARGET="$OPTARG"
 			;;
 		r)
 			ROOTFS_PATH="$OPTARG"
 			;;
-		b)
-			BOARD="$OPTARG"
-			;;
 	esac
 done
 OPTIND=$OLD_OPTIND
 
-if [ ! -f "${EXTLINUXPATH}/${CHIP}.conf" ]; then
-	CHIP="rk3288"
-fi
-
-if [ ! $CHIP ] && [ ! $TARGET ]; then
-	usage
-	exit
-fi
-
-if [[ "${CHIP}" == "rk3308" ]]; then
-	source $LOCALPATH/build/rockpis-partitions.sh
-fi
-
-generate_boot_image() {
+generate_boot_image() 
+{
 	BOOT=${OUT}/boot.img
 	rm -rf ${BOOT}
 
 	echo -e "\e[36m Generate Boot image start\e[0m"
 
-	if [[ "${CHIP}" == "rk3308" ]]; then
-		# 100MB
-		mkfs.vfat -n "boot" -S 512 -C ${BOOT} $((100 * 1024))
-	else
-		# 500Mb
-		mkfs.vfat -n "boot" -S 512 -C ${BOOT} $((500 * 1024))
-	fi
+	# 500Mb
+	mkfs.vfat -n "boot" -S 512 -C ${BOOT} $((500 * 1024))
 
 	mmd -i ${BOOT} ::/extlinux
-	if [ "${BOARD}" == "rockpi4a" ] || [ "${BOARD}" == "rockpi4b" ] ||  [ "${BOARD}" == "rockpis" ] ; then
-		mmd -i ${BOOT} ::/overlays
-	fi
+	mmd -i ${BOOT} ::/overlays
 
-	mcopy -i ${BOOT} -s ${EXTLINUXPATH}/${CHIP}.conf ::/extlinux/extlinux.conf
+	mcopy -i ${BOOT} -s ${EXTLINUXPATH}/rk3399.conf ::/extlinux/extlinux.conf
 	mcopy -i ${BOOT} -s ${OUT}/kernel/* ::
 
 	echo -e "\e[36m Generate Boot image : ${BOOT} success! \e[0m"
 }
 
-generate_system_image() {
+generate_system_image() 
+{
 	if [ ! -f "${OUT}/boot.img" ]; then
 		echo -e "\e[31m CAN'T FIND BOOT IMAGE \e[0m"
 		usage
@@ -107,33 +81,19 @@ generate_system_image() {
 
 	dd if=/dev/zero of=${SYSTEM} bs=1M count=0 seek=$GPT_IMAGE_SIZE
 
-	if [ "$BOARD" == "rockpi4" ]; then
-		parted -s ${SYSTEM} mklabel gpt
-		parted -s ${SYSTEM} unit s mkpart loader1 ${LOADER1_START} $(expr ${RESERVED1_START} - 1)
-		# parted -s ${SYSTEM} unit s mkpart reserved1 ${RESERVED1_START} $(expr ${RESERVED2_START} - 1)
-		# parted -s ${SYSTEM} unit s mkpart reserved2 ${RESERVED2_START} $(expr ${LOADER2_START} - 1)
-		parted -s ${SYSTEM} unit s mkpart loader2 ${LOADER2_START} $(expr ${ATF_START} - 1)
-		parted -s ${SYSTEM} unit s mkpart trust ${ATF_START} $(expr ${BOOT_START} - 1)
-		parted -s ${SYSTEM} unit s mkpart boot ${BOOT_START} $(expr ${ROOTFS_START} - 1)
-		parted -s ${SYSTEM} set 4 boot on
-		parted -s ${SYSTEM} -- unit s mkpart rootfs ${ROOTFS_START} -34s
-	else
-		parted -s ${SYSTEM} mklabel gpt
-		parted -s ${SYSTEM} unit s mkpart boot ${BOOT_START} $(expr ${ROOTFS_START} - 1)
-		parted -s ${SYSTEM} set 1 boot on
-		parted -s ${SYSTEM} -- unit s mkpart rootfs ${ROOTFS_START} -34s
-	fi
+	parted -s ${SYSTEM} mklabel gpt
+	parted -s ${SYSTEM} unit s mkpart loader1 ${LOADER1_START} $(expr ${RESERVED1_START} - 1)
+	# parted -s ${SYSTEM} unit s mkpart reserved1 ${RESERVED1_START} $(expr ${RESERVED2_START} - 1)
+	# parted -s ${SYSTEM} unit s mkpart reserved2 ${RESERVED2_START} $(expr ${LOADER2_START} - 1)
+	parted -s ${SYSTEM} unit s mkpart loader2 ${LOADER2_START} $(expr ${ATF_START} - 1)
+	parted -s ${SYSTEM} unit s mkpart trust ${ATF_START} $(expr ${BOOT_START} - 1)
+	parted -s ${SYSTEM} unit s mkpart boot ${BOOT_START} $(expr ${ROOTFS_START} - 1)
+	parted -s ${SYSTEM} set 4 boot on
+	parted -s ${SYSTEM} -- unit s mkpart rootfs ${ROOTFS_START} -34s
 
-	if [ "$CHIP" == "rk3328" ] || [ "$CHIP" == "rk3399" ] || [ "$CHIP" == "rk3399pro" ]; then
-		ROOT_UUID="B921B045-1DF0-41C3-AF44-4C6F280D3FAE"
-	elif [ "$CHIP" == "rk3308" ] || [ "$CHIP" == "px30" ]; then
-		ROOT_UUID="614e0000-0000-4b53-8000-1d28000054a9"
-	else
-		ROOT_UUID="69DAD710-2CE4-4E3C-B16C-21A1D49ABED3"
-	fi
+	ROOT_UUID="B921B045-1DF0-41C3-AF44-4C6F280D3FAE"
 
-	if [ "$BOARD" == "rockpi4" ]; then
-		gdisk ${SYSTEM} <<EOF
+	gdisk ${SYSTEM} <<EOF
 x
 c
 5
@@ -141,30 +101,12 @@ ${ROOT_UUID}
 w
 y
 EOF
-	else
-		gdisk ${SYSTEM} <<EOF
-x
-c
-2
-${ROOT_UUID}
-w
-y
-EOF
-	fi
+
 
 	# burn u-boot
-	case ${CHIP} in
-	rk322x | rk3036 )
-		dd if=${OUT}/u-boot/idbloader.img of=${SYSTEM} seek=${LOADER1_START} conv=notrunc
-		;;
-	px30 | rk3288 | rk3308 | rk3328 | rk3399 | rk3399pro )
-		dd if=${OUT}/u-boot/idbloader.img of=${SYSTEM} seek=${LOADER1_START} conv=notrunc
-		dd if=${OUT}/u-boot/uboot.img of=${SYSTEM} seek=${LOADER2_START} conv=notrunc
-		dd if=${OUT}/u-boot/trust.img of=${SYSTEM} seek=${ATF_START} conv=notrunc
-		;;
-	*)
-		;;
-	esac
+	dd if=${OUT}/u-boot/idbloader.img of=${SYSTEM} seek=${LOADER1_START} conv=notrunc
+	dd if=${OUT}/u-boot/uboot.img of=${SYSTEM} seek=${LOADER2_START} conv=notrunc
+	dd if=${OUT}/u-boot/trust.img of=${SYSTEM} seek=${ATF_START} conv=notrunc
 
 	# burn boot image
 	dd if=${OUT}/boot.img of=${SYSTEM} conv=notrunc seek=${BOOT_START}
--- build/mk-kernel.sh
+++ build/mk-kernel.sh
@@ -3,7 +3,6 @@
 LOCALPATH=$(pwd)
 OUT=${LOCALPATH}/out
 EXTLINUXPATH=${LOCALPATH}/build/extlinux
-BOARD=$1
 
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 
@@ -13,14 +12,18 @@ finish() {
 }
 trap finish ERR
 
-if [ $# != 1 ]; then
-	BOARD=rk3288-evb
-fi
-
 [ ! -d ${OUT} ] && mkdir ${OUT}
 [ ! -d ${OUT}/kernel ] && mkdir ${OUT}/kernel
 
-source $LOCALPATH/build/board_configs.sh $BOARD
+#source $LOCALPATH/build/board_configs.sh $BOARD
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+BOARD="rockpi4b"
+DEFCONFIG=rockchip_linux_defconfig
+UBOOT_DEFCONFIG=rock-pi-4b-rk3399_defconfig
+DTB=rk3399-rock-pi-4b.dtb
+KERNELIMAGE=""
+CHIP="rk3399"
 
 if [ $? -ne 0 ]; then
 	exit
@@ -31,34 +34,19 @@ echo -e "\e[36m Building kernel for ${BOARD} board! \e[0m"
 KERNEL_VERSION=$(cd ${LOCALPATH}/kernel && make kernelversion)
 echo $KERNEL_VERSION
 
-if version_gt "${KERNEL_VERSION}" "4.5"; then
-	if [ "${DTB_MAINLINE}" ]; then
-		DTB=${DTB_MAINLINE}
-	fi
-
-	if [ "${DEFCONFIG_MAINLINE}" ]; then
-		DEFCONFIG=${DEFCONFIG_MAINLINE}
-	fi
-fi
-
 cd ${LOCALPATH}/kernel
 [ ! -e .config ] && echo -e "\e[36m Using ${DEFCONFIG} \e[0m" && make ${DEFCONFIG}
 
 make -j8
 cd ${LOCALPATH}
 
-if [ "${ARCH}" == "arm" ]; then
-	cp ${LOCALPATH}/kernel/arch/arm/boot/zImage ${OUT}/kernel/
-	cp ${LOCALPATH}/kernel/arch/arm/boot/dts/${DTB} ${OUT}/kernel/
-else
-	cp ${LOCALPATH}/kernel/arch/arm64/boot/Image ${OUT}/kernel/
-	cp ${LOCALPATH}/kernel/arch/arm64/boot/dts/rockchip/${DTB} ${OUT}/kernel/
-fi
+cp ${LOCALPATH}/kernel/arch/arm64/boot/Image ${OUT}/kernel/
+cp ${LOCALPATH}/kernel/arch/arm64/boot/dts/rockchip/${DTB} ${OUT}/kernel/
 
 # Change extlinux.conf according board
 sed -e "s,fdt .*,fdt /$DTB,g" \
 	-i ${EXTLINUXPATH}/${CHIP}.conf
 
-./build/mk-image.sh -c ${CHIP} -t boot -b ${BOARD}
+./build/mk-image.sh -t boot
 
 echo -e "\e[36m Kernel build success! \e[0m"
